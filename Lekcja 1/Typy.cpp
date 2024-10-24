c637c8c4c0d6f112891bbacfe6bdf66a59ffc617
Jakub Sworobowicz (Uczeń) <jsworobowicz705@technikumkreatywne.pl>
2024-10-24 09:14:13 +0200

    dodano 2 pliki z leckji

---
+++ Lekcja 1/Typy.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int liczbacalkowita= 10;
+	float liczba_zmiennoprzecinkowa = 3.14;
+	char pojedynczyznak = 'A';
+	bool wartosclogiczna = true;
+	
+	cout << "Typ int: " << liczbacalkowita << endl;
+	cout << "Typ float: " << liczba_zmiennoprzecinkowa << endl;
+	cout << "Typ char: " << pojedynczyznak << endl;
+	cout << "Typ bool: " << wartosclogiczna << endl;
+	
+}
